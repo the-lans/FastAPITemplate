6e209399198415e5460d991f9aa607346c588ecd
Ilya Vishinsky <ivyishinskiy@cloudlinux.com>
2021-07-20 18:04:55 +0300

    Api Foo

--- backend/api/foo.py
+++ backend/api/foo.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from fastapi import Depends
@@ -14,18 +13,32 @@ router = InferringRouter()
 @cbv(router)
 class Foo:
     current_user: User = Depends(get_current_active_user)
+    data: dict = {}
+    item_id: int = 0
 
     @router.get("/", tags=["Foo"])
-    async def read_root(self):
+    async def get_root(self):
         return {"Hello": "World"}
 
+    @router.get("/items/list/?", tags=["Foo"])
+    async def get_list(self):
+        return self.data
+
     @router.get("/items/{item_id}", tags=["Foo"])
-    async def read_item(self, item_id: int, q: Optional[str] = None):
-        return {"item_id": item_id, "q": q}
+    async def get_item(self, item_id: int):
+        return {item_id: self.data[item_id]}
+
+    @router.post("/items/new", tags=["Foo"])
+    async def new_item(self, item: Item):
+        self.data[self.item_id] = item
+        res = {"item_id": self.item_id}
+        self.item_id += 1
+        return res
 
     @router.put("/items/{item_id}", tags=["Foo"])
     async def update_item(self, item_id: int, item: Item):
-        return {"item_name": item.name, "item_id": item_id}
+        self.data[item_id] = item
+        return {"item_id": item_id}
 
 
 app.include_router(router)
