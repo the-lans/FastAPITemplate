51c25622944b26fde545fec1824b2e334b020662
Ilya Vishinsky <ivyishinskiy@cloudlinux.com>
2021-08-16 17:59:52 +0300

    config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
+from backend.config import DB_SETTINGS
 from backend.api import *
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=DB_SETTINGS['DOMAIN'], port=DB_SETTINGS['PORT'])
